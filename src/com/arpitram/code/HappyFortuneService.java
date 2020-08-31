0e909b620d9aa85ab6cc384a3a456a9887d68964
Arpit Gupta <imarg_3@zoho.com>
2020-08-31 23:22:42 +0530

    Add FortuneService for Constructor & Setter Injection

---
+++ src/com/arpitram/code/HappyFortuneService.java
@@ -0,0 +1,10 @@
+package com.arpitram.code;
+
+public class HappyFortuneService implements FortuneService {
+
+	@Override
+	public String getFortune() {
+		return "Today is your lucky day!";
+	}
+
+}
