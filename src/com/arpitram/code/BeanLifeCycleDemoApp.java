1b3bbcd2c98419e76ab41598c6a657b1eccedec5
Arpit Gupta <imarg_3@zoho.com>
2020-09-05 00:13:15 +0530

    Spring Config Bean LifeCycle

---
+++ src/com/arpitram/code/BeanLifeCycleDemoApp.java
@@ -0,0 +1,20 @@
+package com.arpitram.code;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanLifeCycleDemoApp {
+
+	public static void main(String[] args) {
+		// load the spring configuration file
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beanLifeCycle-applicationContext.xml");
+		
+		// retrieve bean from spring container
+		Coach coach = context.getBean("myCoach", Coach.class); // singleton object
+		
+		System.out.println(coach.getDailyWorkout());
+		
+		// close the context
+		context.close();
+	}
+
+}
