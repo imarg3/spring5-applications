ca815c37fe6e18c2ad7f05e00200f4260426206a
Arpit Gupta <imarg_3@zoho.com>
2020-09-05 00:16:16 +0530

    Add Spring Container lifecycle methods

--- src/com/arpitram/code/TrackCoach.java
+++ src/com/arpitram/code/TrackCoach.java
@@ -18,5 +18,16 @@ public class TrackCoach implements Coach {
 	public String getDailyFortune() {
 		return "Just Do It : " + fortuneService.getFortune();
 	}
+	
+	// add an init method
+	public void doMyStartupStuff() {
+		System.out.println("TrackCoach: inside doMyStartupStaff method");
+	}
+	
+	// add a destroy method
+	// NOTE: For prototype scope, destroy-method will not gets called
+	public void doMyCleanupStuff() {
+		System.out.println("TrackCoach: inside doMyCleanupStaff method");
+	}
 
 }
